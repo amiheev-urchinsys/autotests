b7fcdfd379da2c9d73b42a3154a05eeea8623b4f
Alexandr Miheev <amiheev@urchinsys.com>
2025-07-01 18:08:05 +0300

    some tests

--- pageObjects/hubsPage.py
+++ pageObjects/hubsPage.py
@@ -15,5 +15,5 @@ class HubsPage(BasePage):
         self.page_title = page.locator('div[class="page-content"] span[class="name"]')
         self.create_a_hub_button = page.get_by_role("button", name="Create A Hub")
         self.outline_based_type_card = page.locator('div[class="menu"] div[class="menu-item"]:nth-child(1)')
-
+        self.create_a_hub_next_button = page.get_by_role("button", name="Next")
 
--- test_hubs_page.py
+++ test_hubs_page.py
@@ -1,10 +1,15 @@
+import time
+
+import pytest
 from playwright.sync_api import Playwright, expect
 
 from pageObjects.documentsInsightsPage import DocumentsInsightsPage
 from pageObjects.homePage import HomePage
-from pageObjects.loginPage import LoginPage
 from utilities.api.api_base import get_user_token
 
+
+@pytest.mark.hubs
+@pytest.mark.outline_based
 def test_create_an_outline_based_hub(playwright: Playwright):
     # Get user token to set the cookies
     response = get_user_token(playwright)
@@ -23,7 +28,7 @@ def test_create_an_outline_based_hub(playwright: Playwright):
         "sameSite": "Lax"
     }])
     page = context.new_page()
-    # Test
+    # Steps
     page.goto("https://studio.dev.plextera.com")
     on_home_page = HomePage(page)
     on_home_page.sidebar.sidebar_bottom_section.hover()
@@ -32,3 +37,10 @@ def test_create_an_outline_based_hub(playwright: Playwright):
     on_hubs_page = on_documents_insights_page.navigate_to_hubs_page()
     on_hubs_page.create_a_hub_button.click()
     on_hubs_page.outline_based_type_card.click()
+    # Wait until after the click on the Next button the '/api/hubs/default-name' request will be finished successfully
+    with page.expect_response("**/api/hubs/default-name") as resp_info:
+        on_hubs_page.create_a_hub_next_button.click()
+    response = resp_info.value
+    assert response.ok
+    on_hubs_page.create_a_hub_next_button.click()
+
